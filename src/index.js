bb72ecbf3654eb49ad92176309224a49c8dac244
jberg <jordannealberg@gmail.com>
2018-05-04 16:31:28 -0700

    fix gl support detection

--- src/index.js
+++ src/index.js
@@ -6,18 +6,23 @@ export default class Butterchurn {
   static createVisualizer (context, canvas, opts) {
     return new Visualizer(context, canvas, opts);
   }
-}
 
-export function isVisualizerSupported () {
-  const canvas = document.createElement('canvas');
-  let gl;
-  try {
-    gl = canvas.getContext('webgl2');
-  } catch (x) {
-    gl = null;
+  static isVisualizerSupported () {
+    const canvas = document.createElement('canvas');
+    let gl;
+    try {
+      gl = canvas.getContext('webgl2');
+    } catch (x) {
+      gl = null;
+    }
+
+    const webGL2Supported = !!gl;
+    const audioApiSupported = !!(window.AudioContext || window.webkitAudioContext);
+
+    return webGL2Supported && audioApiSupported;
   }
-
-  return !!gl;
 }
+
+export const isVisualizerSupported = Butterchurn.isVisualizerSupported;
 
 module.exports = Butterchurn;
