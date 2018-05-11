7df4522b369ed7b5c473ac7abc07da7d3115687b
jberg <jordannealberg@gmail.com>
2018-05-10 21:01:54 -0700

    create functions for all text JSON presets

--- src/visualizer.js
+++ src/visualizer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import AudioProcessor from './audio/audioProcessor';
 import Renderer from './rendering/renderer';
 
@@ -26,6 +27,39 @@ export default class Visualizer {
   }
 
   loadPreset (preset, blendTime) {
+    if (!_.isFunction(preset.init_eqs)) {
+      /* eslint-disable no-param-reassign, no-new-func */
+      preset.init_eqs = new Function('m', `${preset.init_eqs_str} return m;`);
+      preset.frame_eqs = new Function('m', `${preset.frame_eqs_str} return m;`);
+      if (preset.pixel_eqs_str && preset.pixel_eqs_str !== '') {
+        preset.pixel_eqs = new Function('m', `${preset.pixel_eqs_str} return m;`);
+      } else {
+        preset.pixel_eqs = '';
+      }
+
+      for (let i = 0; i < preset.shapes.length; i++) {
+        preset.shapes[i] = _.assign(preset.shapes[i], {
+          init_eqs: new Function('m', `${preset.shapes[i].init_eqs_str} return m;`),
+          frame_eqs: new Function('m', `${preset.shapes[i].frame_eqs_str} return m;`),
+        });
+      }
+
+      for (let i = 0; i < preset.waves.length; i++) {
+        const wave = {
+          init_eqs: new Function('m', `${preset.waves[i].init_eqs_str} return m;`),
+          frame_eqs: new Function('m', `${preset.waves[i].frame_eqs_str} return m;`),
+        };
+
+        if (preset.waves[i].point_eqs_str && preset.waves[i].point_eqs_str !== '') {
+          wave.point_eqs = new Function('m', `${preset.waves[i].point_eqs_str} return m;`);
+        } else {
+          wave.point_eqs = '';
+        }
+
+        preset.waves[i] = _.assign(preset.waves[i], wave);
+      }
+      /* eslint-enable no-param-reassign, no-new-func */
+    }
     this.renderer.loadPreset(preset, blendTime);
   }
 
