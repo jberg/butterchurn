2bc8bbccbfc8007cf7ceb6a9e5f29f29f119e428
jberg <jordannealberg@gmail.com>
2018-05-26 10:28:08 -0700

    use parallel uglify

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -72,7 +72,7 @@ if (env === 'prod') {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
 
-    new UglifyJsPlugin()
+    new UglifyJsPlugin({ parallel: true })
   );
 }
 
