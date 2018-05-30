9c4a9396962c589666224f7f14d9557dbdd98f03
jberg <jordannealberg@gmail.com>
2018-05-30 15:28:37 -0700

    remove unneeded default values

--- src/rendering/renderer.js
+++ src/rendering/renderer.js
@@ -1,6 +1,4 @@
-import _ from 'lodash';
 import AudioLevels from '../audio/audioLevels';
-
 import blankPreset from '../blankPreset';
 import PresetEquationRunner from '../equations/presetEquationRunner';
 import BasicWaveform from './waves/basicWaveform';
@@ -630,28 +628,24 @@ export default class Renderer {
     mixedFrame.brighten = (mix < snapPoint) ? mdVSFramePrev.brighten : mdVSFrame.brighten;
     mixedFrame.darken = (mix < snapPoint) ? mdVSFramePrev.darken : mdVSFrame.darken;
     mixedFrame.solarize = (mix < snapPoint) ? mdVSFramePrev.brighten : mdVSFrame.solarize;
-    mixedFrame.b1n = (mix * (mdVSFrame.b1n || 0)) + (mix2 * (mdVSFramePrev.b1n || 0));
-    mixedFrame.b2n = (mix * (mdVSFrame.b2n || 0)) + (mix2 * (mdVSFramePrev.b2n || 0));
-    mixedFrame.b3n = (mix * (mdVSFrame.b3n || 0)) + (mix2 * (mdVSFramePrev.b3n || 0));
-    mixedFrame.b1x = (mix * (_.get(mdVSFrame, 'b1x', 1))) +
-                     (mix2 * (_.get(mdVSFramePrev, 'b1x', 1)));
-    mixedFrame.b2x = (mix * (_.get(mdVSFrame, 'b2x', 1))) +
-                     (mix2 * (_.get(mdVSFramePrev, 'b2x', 1)));
-    mixedFrame.b3x = (mix * (_.get(mdVSFrame, 'b3x', 1))) +
-                     (mix2 * (_.get(mdVSFramePrev, 'b3x', 1)));
-    mixedFrame.b1ed = (mix * (_.get(mdVSFrame, 'b1ed', 0.25))) +
-                      (mix2 * (_.get(mdVSFramePrev, 'b1ed', 0.25)));
+    mixedFrame.b1n = (mix * mdVSFrame.b1n) + (mix2 * mdVSFramePrev.b1n);
+    mixedFrame.b2n = (mix * mdVSFrame.b2n) + (mix2 * mdVSFramePrev.b2n);
+    mixedFrame.b3n = (mix * mdVSFrame.b3n) + (mix2 * mdVSFramePrev.b3n);
+    mixedFrame.b1x = (mix * mdVSFrame.b1x) + (mix2 * mdVSFramePrev.b1x);
+    mixedFrame.b2x = (mix * mdVSFrame.b2x) + (mix2 * mdVSFramePrev.b2x);
+    mixedFrame.b3x = (mix * mdVSFrame.b3x) + (mix2 * mdVSFramePrev.b3x);
+    mixedFrame.b1ed = (mix * mdVSFrame.b1ed) + (mix2 * mdVSFramePrev.b1ed);
 
     return mixedFrame;
   }
 
   static getBlurValues (mdVSFrame) {
-    let blurMin1 = mdVSFrame.b1n || 0;
-    let blurMin2 = mdVSFrame.b2n || 0;
-    let blurMin3 = mdVSFrame.b3n || 0;
-    let blurMax1 = _.get(mdVSFrame, 'b1x', 1);
-    let blurMax2 = _.get(mdVSFrame, 'b2x', 1);
-    let blurMax3 = _.get(mdVSFrame, 'b3x', 1);
+    let blurMin1 = mdVSFrame.b1n;
+    let blurMin2 = mdVSFrame.b2n;
+    let blurMin3 = mdVSFrame.b3n;
+    let blurMax1 = mdVSFrame.b1x;
+    let blurMax2 = mdVSFrame.b2x;
+    let blurMax3 = mdVSFrame.b3x;
 
     const fMinDist = 0.1;
     if ((blurMax1 - blurMin1) < fMinDist) {
