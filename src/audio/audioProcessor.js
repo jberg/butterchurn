6259d01cf670ed7e9cfe23cca9ce9da12e27ffbc
z <>
2019-03-05 20:27:25 +1100

    Test fullwidth FFT

--- src/audio/audioProcessor.js
+++ src/audio/audioProcessor.js
@@ -3,8 +3,9 @@ import FFT from './fft';
 export default class AudioProcessor {
   constructor (context) {
     this.numSamps = 512;
+    this.fftSize = this.numSamps * 2;
 
-    this.fft = new FFT(this.numSamps, 512);
+    this.fft = new FFT(this.fftSize, 512);
 
     if (context) {
       this.audioContext = context;
@@ -12,58 +13,73 @@ export default class AudioProcessor {
 
       this.analyser = context.createAnalyser();
       this.analyser.smoothingTimeConstant = 0.0;
-      this.analyser.fftSize = this.numSamps * 2;
+      this.analyser.fftSize = this.fftSize;
 
       this.audible.connect(this.analyser);
 
       // Split channels
       this.analyserL = context.createAnalyser();
       this.analyserL.smoothingTimeConstant = 0.0;
-      this.analyserL.fftSize = this.numSamps * 2;
+      this.analyserL.fftSize = this.fftSize;
 
       this.analyserR = context.createAnalyser();
       this.analyserR.smoothingTimeConstant = 0.0;
-      this.analyserR.fftSize = this.numSamps * 2;
+      this.analyserR.fftSize = this.fftSize;
 
       this.splitter = context.createChannelSplitter(2);
 
       this.audible.connect(this.splitter);
       this.splitter.connect(this.analyserL, 0);
       this.splitter.connect(this.analyserR, 1);
+
+      // New Stuff
+      this.timeByteArray = new Uint8Array(this.fftSize);
+      this.timeByteArrayL = new Uint8Array(this.fftSize);
+      this.timeByteArrayR = new Uint8Array(this.fftSize);
+
+      this.timeArray = new Int8Array(this.fftSize);
+      this.timeByteArraySignedL = new Int8Array(this.fftSize);
+      this.timeByteArraySignedR = new Int8Array(this.fftSize);
+
+
+      this.timeArrayL = new Int8Array(this.numSamps);
+      this.timeArrayR = new Int8Array(this.numSamps);
     }
   }
 
   sampleAudio () {
-    const timeByteArray = new Uint8Array(this.numSamps);
-    const timeByteArrayL = new Uint8Array(this.numSamps);
-    const timeByteArrayR = new Uint8Array(this.numSamps);
-    this.analyser.getByteTimeDomainData(timeByteArray);
-    this.analyserL.getByteTimeDomainData(timeByteArrayL);
-    this.analyserR.getByteTimeDomainData(timeByteArrayR);
+    this.analyser.getByteTimeDomainData(this.timeByteArray);
+    this.analyserL.getByteTimeDomainData(this.timeByteArrayL);
+    this.analyserR.getByteTimeDomainData(this.timeByteArrayR);
 
-    this.updateAudio(timeByteArray, timeByteArrayL, timeByteArrayR);
+    this.updateAudio();
   }
 
-  updateAudio (timeByteArray, timeByteArrayL, timeByteArrayR) {
-    this.timeArray = [];
-    this.timeArrayL = [];
-    this.timeArrayR = [];
-    const tempTimeL = [];
-    const tempTimeR = [];
-    let lastIdx = 0;
-    for (let i = 0; i < this.numSamps; i++) {
-      this.timeArray.push(timeByteArray[i] - 128);
-      this.timeArrayL.push(timeByteArrayL[i] - 128);
-      this.timeArrayR.push(timeByteArrayR[i] - 128);
+  updateAudio () {
+    // let lastIdx = 0;
+    for (let i = 0, j=0; i < this.fftSize; i++) {
+      // Unsigned to Signed
+      this.timeArray[i] = this.timeByteArray[i] - 128;
+      this.timeByteArraySignedL[i] = this.timeByteArrayL[i] - 128;
+      this.timeByteArraySignedR[i] = this.timeByteArrayR[i] - 128;
 
-      tempTimeL[i] = 0.5 * (this.timeArrayL[i] + this.timeArrayL[lastIdx]);
-      tempTimeR[i] = 0.5 * (this.timeArrayR[i] + this.timeArrayR[lastIdx]);
-      lastIdx = i;
+      // Undersampled
+      if (i & 2) { // Equivalent to i % 2
+        this.timeArrayL[j] = this.timeByteArraySignedL[i];
+        this.timeArrayR[j] = this.timeByteArraySignedR[i];
+        j++;
+      }
+
+      // Test no smoothing
+      // tempTimeL[i] = 0.5 * (this.timeArrayL[i] + this.timeArrayL[lastIdx]);
+      // tempTimeR[i] = 0.5 * (this.timeArrayR[i] + this.timeArrayR[lastIdx]);
+      // lastIdx = i;
     }
 
+    // Use full width samples for the FFT
     this.freqArray = this.fft.timeToFrequencyDomain(this.timeArray);
-    this.freqArrayL = this.fft.timeToFrequencyDomain(tempTimeL);
-    this.freqArrayR = this.fft.timeToFrequencyDomain(tempTimeR);
+    this.freqArrayL = this.fft.timeToFrequencyDomain(this.timeByteArraySignedL);
+    this.freqArrayR = this.fft.timeToFrequencyDomain(this.timeByteArraySignedR);
   }
 
   connectAudio (audionode) {
