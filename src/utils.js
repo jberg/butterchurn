b8cdf66eaeb5f7861243c6c07a2cded2c7cb8367
jberg <jberg@users.noreply.github.com>
2020-09-13 16:49:52 -0700

    enforce unset variables are 0 in JS presets

--- src/utils.js
+++ src/utils.js
@@ -23,7 +23,7 @@ export default class Utils {
     const newObj = {};
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
-      newObj[key] = obj[key];
+      newObj[key] = obj[key] || 0;
     }
     return newObj;
   }
